63497a20cd92d8d2dfad11309ac17cffd9deb0ef
Hugo Ribeira <hugoribeira@gmail.com>
2020-11-28 13:22:59 +0000

    Show engine runs in red

--- src/maps/flight_renderer.ts
+++ src/maps/flight_renderer.ts
@@ -4,13 +4,22 @@ import LineString from "ol/geom/LineString";
 import Point from "ol/geom/Point";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
 import { Fill, Stroke, Style } from "ol/style";
 import CircleStyle from "ol/style/Circle";
+import { splitWhen } from "../utils/arrays";
 import Map from "./map";
 import { TrackColors } from "./track_colors";
 import { positionToOlPoint } from "./utils";
 
 export const COLORS = new TrackColors();
+
+type TrackSegment = {
+  startIndex: number;
+  endIndex: number;
+  isEngineOn: boolean;
+  positions: number[][];
+};
 
 interface Options {
   renderFullTrack?: boolean;
@@ -19,23 +28,66 @@ interface Options {
 export default class FlightRenderer {
   private flight: SavedFlight;
   private map: Map;
-  private flightTrackFeature: any;
-  private positionMarkerFeature: any;
+  private trackSegments: TrackSegment[];
+  private trackSegmentFeatures?: Feature<LineString>[];
+  private positionMarkerFeature?: Feature<Point>;
   private renderFullTrack: boolean;
-  private flightPositions: number[][];
 
   constructor(map: Map, flight: SavedFlight, options: Options = {}) {
     this.map = map;
     this.flight = flight;
-    this.flightPositions = flight.getDatums().map((d) => this.fixToPoint(d));
+    this.trackSegments = this.buildTrackSegments(flight);
+    console.log(
+      this.trackSegments.length,
+      this.trackSegments[0].startIndex,
+      this.trackSegments[0].endIndex,
+      this.trackSegments[1].startIndex,
+      this.trackSegments[1].endIndex,
+      this.trackSegments[2].startIndex,
+      this.trackSegments[2].endIndex,
+      this.flight.getDatums().length
+    );
     this.renderFullTrack = options.renderFullTrack || false;
   }
 
+  private buildTrackSegments(flight: SavedFlight) {
+    const datumSlices = splitWhen(
+      flight.getDatums(),
+      (datum) => this.isEngineOn(datum),
+      {
+        includeLastValueInBothGroups: true,
+      }
+    );
+
+    let startIndex = 0;
+    return datumSlices.map((datums) => {
+      const isEngineOn = this.isEngineOn(datums[0]);
+      const positions = datums.map((datum: Datum) => this.fixToPoint(datum));
+      const endIndex = startIndex + positions.length - 1;
+      const segment = {
+        startIndex: startIndex,
+        endIndex: endIndex,
+        positions: positions,
+        isEngineOn: isEngineOn,
+      };
+
+      startIndex = endIndex;
+
+      return segment;
+    });
+  }
+
+  private isEngineOn(datum: Datum) {
+    return datum.calculatedValues.engineOn?.value === 1;
+  }
+
   render() {
-    this.flightTrackFeature = this.buildFlightTrackFeature();
+    this.trackSegmentFeatures = this.buildTrackSegmentFeatures();
     this.positionMarkerFeature = this.buildPositionMarkerFeature();
     let source = new VectorSource({
-      features: [this.flightTrackFeature, this.positionMarkerFeature],
+      features: this.trackSegmentFeatures.concat(
+        this.positionMarkerFeature as any
+      ),
     });
 
     let color = COLORS.getColorFor(this.flight);
@@ -62,32 +114,63 @@ export default class FlightRenderer {
   }
 
   private drawFlightTrackUntil(timestamp: Date) {
-    let currentDatumIndex = this.flight.datumIndexAt(timestamp);
-    if (currentDatumIndex === null) return;
+    if (!this.trackSegmentFeatures) return;
 
-    const positions = this.flightPositions.slice(0, currentDatumIndex + 1);
-    this.flightTrackFeature.getGeometry().setCoordinates(positions);
+    let currentDatumIndex = this.flight.datumIndexAt(timestamp);
+
+    this.trackSegmentFeatures.forEach((segment, index) => {
+      if (currentDatumIndex === null) return;
+
+      const trackSegment = this.trackSegments[index];
+      let positions: number[][];
+
+      if (currentDatumIndex < trackSegment.startIndex) {
+        positions = [];
+      } else if (currentDatumIndex >= trackSegment.endIndex) {
+        positions = trackSegment.positions;
+      } else {
+        positions = trackSegment.positions.slice(
+          0,
+          currentDatumIndex - trackSegment.startIndex
+        );
+      }
+
+      segment.getGeometry()!.setCoordinates(positions);
+    });
   }
 
   private updateMarkerPosition(datum: Datum) {
+    if (!this.positionMarkerFeature) return;
+
     this.positionMarkerFeature
-      .getGeometry()
+      .getGeometry()!
       .setCoordinates(this.fixToPoint(datum));
   }
 
-  private buildFlightTrackFeature() {
-    const { Feature } = this.map.ol;
-    let points = this.flight
-      .getDatums()
-      .map((datum: Datum) => this.fixToPoint(datum));
-    let line = new LineString(points);
-    let feature = new Feature({ geometry: line });
-    return feature;
+  private buildTrackSegmentFeatures() {
+    return this.trackSegments.map((segment) => {
+      const isEngineOn = segment.isEngineOn;
+      const line = new LineString(segment.positions);
+      const feature = new Feature<LineString>({ geometry: line });
+      feature.setStyle(this.traceStyle(isEngineOn));
+      return feature;
+    });
+  }
+
+  private traceStyle(isEngineOn: boolean) {
+    if (!isEngineOn) return;
+    return [
+      new Style({
+        stroke: new Stroke({
+          color: "#FF0000",
+          width: 2,
+        }),
+      }),
+    ];
   }
 
   private buildPositionMarkerFeature() {
-    const { Feature } = this.map.ol;
-    return new Feature({
+    return new Feature<Point>({
       geometry: new Point(this.fixToPoint(this.flight.getDatums()[0])),
     });
   }

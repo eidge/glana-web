b9165662dfe142905385de1c61776c3a94f38b8c
Hugo Ribeira <hugoribeira@gmail.com>
2020-12-26 18:45:55 +0000

    Handle errors

---
+++ pages/_error.tsx
@@ -0,0 +1,28 @@
+import Div100vh from "react-div-100vh";
+import Modal, { ModalBody } from "../src/components/ui/modal";
+
+export default function Error() {
+  return (
+    <Div100vh className="bg-yellow-100">
+      <Modal isOpen={true} onClose={() => {}}>
+        <ModalBody>
+          <div>
+            <h1 className="text-xl font-semibold mb-4">
+              Flight could not be loaded.
+            </h1>
+            <div className="mt-4">
+              <span className="text-gray-700">
+                You can manually upload a flight{" "}
+                <a href="/" className="text-primary">
+                  here
+                </a>
+                .
+              </span>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+    </Div100vh>
+  );
+}
+

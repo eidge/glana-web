eb6a191d945ad2d3e623eb006e210676d1c82413
Hugo Ribeira <hugoribeira@gmail.com>
2021-01-01 15:41:30 +0000

    Render flight with 3D terrain - flight track is flat

--- src/flight_analysis/components/main_screen/mapbox.tsx
+++ src/flight_analysis/components/main_screen/mapbox.tsx
@@ -1,5 +1,5 @@
-import { RefObject, useCallback, useEffect, useRef, useState } from "react";
-import { AnalysisState } from "../../store/reducer";
+import { RefObject, useEffect, useRef, useState } from "react";
+import { AnalysisState, FlightDatum } from "../../store/reducer";
 import Timeline from "./timeline";
 import React from "react";
 
@@ -29,7 +29,9 @@ export default function Mapbox(props: Props) {
   } = props;
 
   const elementRef = useRef<HTMLDivElement | null>(null);
-  useMap(elementRef);
+
+  const map = useMap(elementRef);
+  useRenderFlights(map, analysis);
 
   return (
     <div className="relative w-full h-full bg-gray-800">
@@ -48,16 +50,107 @@ export default function Mapbox(props: Props) {
 }
 
 function useMap(elementRef: RefObject<HTMLElement | null>) {
+  const [map, setMap] = useState<mapboxgl.Map | null>(null);
+
   useEffect(() => {
     const element = elementRef.current;
-    console.log(element);
     if (!element) return;
     const map = new mapboxgl.Map({
       container: element,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [6.5335, 44.2842],
-      zoom: 9,
-      failIfMajorPerformanceCaveat: true
+      style: "mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y",
+      center: [6.359724988491934, 44.43421863689022],
+      pitch: 68,
+      bearing: 34,
+      zoom: 12,
+      failIfMajorPerformanceCaveat: true,
+      attributionControl: false,
+      logoPosition: "top-left"
+    });
+    // @ts-ignore
+    window.map = map;
+    map.on("load", () => {
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        tileSize: 512,
+        maxzoom: 14
+      });
+      // add the DEM source as a terrain layer with exaggerated height
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+      // add a sky layer that will show when the map is highly pitched
+      map.addLayer({
+        id: "sky",
+        // @ts-ignore - Dated types
+        type: "sky",
+        paint: {
+          // @ts-ignore - Dated types
+          "sky-type": "atmosphere",
+          "sky-atmosphere-sun": [0.0, 0.0],
+          "sky-atmosphere-sun-intensity": 15
+        }
+      });
+
+      setMap(map);
     });
   }, [elementRef]);
+
+  return map;
+}
+
+function useRenderFlights(
+  map: mapboxgl.Map | null,
+  analysis: AnalysisState | null
+) {
+  const { followFlightId, flightDataById } = analysis || {};
+  const flight = !!followFlightId && flightDataById![followFlightId];
+
+  useEffect(() => {
+    console.log("koko");
+    if (!map || !flight) return;
+    const geoJson = flightToGeoJson(flight);
+    const sourceId = `source-${flight.id}`;
+    const layerId = `layer-${flight.id}`;
+    console.log(geoJson);
+    map.addSource(sourceId, geoJson);
+    map.addLayer({
+      id: layerId,
+      source: sourceId,
+      type: "line",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      paint: {
+        "line-color": flight.color,
+        "line-width": 2
+      }
+    });
+
+    return () => {
+      map.removeLayer(layerId);
+      map.removeSource(sourceId);
+    };
+  }, [map, flight]);
+}
+
+function flightToGeoJson(flightDatum: FlightDatum): mapboxgl.AnySourceData {
+  const coordinates = flightDatum.flight.datums.map(d => [
+    d.position.longitude.value,
+    d.position.latitude.value
+  ]);
+  return {
+    type: "geojson",
+    data: {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: coordinates
+      },
+      properties: {
+        title: flightDatum.flight.id,
+        color: "red"
+      }
+    }
+  };
 }

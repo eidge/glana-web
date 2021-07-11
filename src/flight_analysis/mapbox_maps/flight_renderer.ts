496963623bbd08403118a21d7394821d0b11bc6b
Hugo Ribeira <hugoribeira@gmail.com>
2021-07-11 14:41:46 +0100

    Move things to its own files

---
+++ src/flight_analysis/mapbox_maps/flight_renderer.ts
@@ -0,0 +1,123 @@
+import { Feature, LineString } from "geojson";
+import { Map, LngLatBounds, GeoJSONSource } from "mapbox-gl";
+import { FlightDatum } from "../store/reducer";
+
+export default class FlightRenderer {
+  private map: Map;
+  private flightDatum: FlightDatum;
+  private sourceId: string;
+  private layerId: string;
+  private geoJSON: GeoJSON.Feature<
+    GeoJSON.LineString,
+    NonNullable<GeoJSON.GeoJsonProperties>
+  >;
+  private bounds: LngLatBounds;
+  private coordinates: GeoJSON.Position[];
+  private isActive: boolean;
+  private shouldRenderFullTracks: boolean;
+  private timestamp: Date;
+
+  constructor(map: Map, flightDatum: FlightDatum) {
+    this.map = map;
+    this.flightDatum = flightDatum;
+    this.timestamp = flightDatum.flight.getRecordingStartedAt();
+    this.sourceId = `source-${this.flightDatum.id}`;
+    this.layerId = `layer-${this.flightDatum.id}`;
+    this.coordinates = this.flightDatum.flight.datums.map(d => [
+      d.position.longitude.value,
+      d.position.latitude.value
+    ]);
+    this.geoJSON = this.buildGeoJSON(this.coordinates);
+    this.bounds = this.calculateBounds(this.geoJSON);
+    this.isActive = true;
+    this.shouldRenderFullTracks = true;
+  }
+
+  private buildGeoJSON(
+    coordinates: GeoJSON.Position[]
+  ): GeoJSON.Feature<
+    GeoJSON.LineString,
+    NonNullable<GeoJSON.GeoJsonProperties>
+  > {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: coordinates
+      },
+      properties: {
+        title: this.flightDatum.flight.id,
+        color: this.flightDatum.color,
+        opacity: this.opacity
+      }
+    };
+  }
+
+  private calculateBounds(geoJSON: Feature<LineString>) {
+    console.log(geoJSON);
+    return geoJSON.geometry.coordinates.reduce(
+      (bounds, coordinate: any) => bounds.extend(coordinate),
+      new LngLatBounds()
+    );
+  }
+
+  initialize() {
+    this.map.addSource(this.sourceId, {
+      type: "geojson",
+      data: this.geoJSON
+    });
+    this.map.addLayer({
+      id: this.layerId,
+      source: this.sourceId,
+      type: "line",
+      paint: {
+        "line-color": ["get", "color"],
+        "line-width": 2,
+        "line-opacity": ["get", "opacity"]
+      }
+    });
+  }
+
+  getBounds() {
+    return this.bounds;
+  }
+
+  setActive(isActive: boolean) {
+    this.isActive = isActive;
+    const source = this.map.getSource(this.sourceId) as GeoJSONSource;
+    this.geoJSON.properties.opacity = this.opacity;
+    source.setData(this.geoJSON);
+  }
+
+  setRenderFullTracks(shouldRenderFullTracks: boolean) {
+    this.shouldRenderFullTracks = shouldRenderFullTracks;
+    this.setTime(this.timestamp);
+  }
+
+  private get opacity() {
+    return this.isActive ? 1 : 0.4;
+  }
+
+  setTime(timestamp: Date) {
+    this.timestamp = timestamp;
+    let coordinates = this.coordinates;
+
+    if (!this.shouldRenderFullTracks) {
+      const idxAt = this.flightDatum.flight.datumIndexAt(timestamp);
+      coordinates = this.coordinates.slice(0, idxAt + 1);
+    }
+
+    if (this.geoJSON.geometry.coordinates === coordinates) {
+      return;
+    }
+
+    const source = this.map.getSource(this.sourceId) as GeoJSONSource;
+    this.geoJSON.geometry.coordinates = coordinates;
+    source.setData(this.geoJSON);
+  }
+
+  destroy() {
+    this.map.removeLayer(this.layerId);
+    this.map.removeSource(this.sourceId);
+  }
+}
---
+++ src/flight_analysis/mapbox_maps/task_renderer.ts
@@ -0,0 +1,88 @@
+import { FeatureCollection } from "geojson";
+import Task from "glana/src/flight_computer/tasks/task";
+import { Map, LngLatBounds } from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default class TaskRenderer {
+  private id;
+  private map: Map;
+  private sourceId: string;
+  private layerContourId: string;
+  private layerFillId: string;
+  private geoJSON: GeoJSON.FeatureCollection;
+  private bounds: LngLatBounds;
+
+  constructor(map: Map, task: Task) {
+    this.id = this.generateId();
+    this.map = map;
+    this.sourceId = `source-${this.id}`;
+    this.layerContourId = `layer-contour-${this.id}`;
+    this.layerFillId = `layer-fill-${this.id}`;
+    this.geoJSON = this.buildGeoJSON(task);
+    this.bounds = this.calculateBounds(this.geoJSON);
+  }
+
+  private generateId() {
+    return Math.round(Math.random() * 1000000).toString();
+  }
+
+  buildGeoJSON(task: Task): GeoJSON.FeatureCollection {
+    return {
+      type: "FeatureCollection",
+      features: task.turnpoints.map(tp => tp.toGeoJSON())
+    };
+  }
+
+  getBounds() {
+    return this.bounds;
+  }
+
+  private calculateBounds(geoJSON: FeatureCollection) {
+    const coordinates = geoJSON.features.flatMap(f => {
+      if (f.geometry.type === "Polygon") {
+        return f.geometry.coordinates[0];
+      } else if (f.geometry.type === "LineString") {
+        return f.geometry.coordinates;
+      } else {
+        return [];
+      }
+    });
+    return coordinates.reduce(
+      (bounds, coordinate: any) => bounds.extend(coordinate),
+      new LngLatBounds()
+    );
+  }
+
+  initialize() {
+    this.map.addSource(this.sourceId, {
+      type: "geojson",
+      data: this.geoJSON,
+      tolerance: 0
+    });
+    this.map.addLayer({
+      id: this.layerContourId,
+      source: this.sourceId,
+      type: "line",
+      paint: {
+        "line-color": "black",
+        "line-width": 2,
+        "line-opacity": 0.3
+      }
+    });
+    this.map.addLayer({
+      id: this.layerFillId,
+      source: this.sourceId,
+      type: "fill",
+      paint: {
+        "fill-color": "black",
+        "fill-opacity": 0.1
+      }
+    });
+  }
+
+  destroy() {
+    this.map.removeLayer(this.layerFillId);
+    this.map.removeLayer(this.layerContourId);
+    this.map.removeSource(this.sourceId);
+  }
+}
